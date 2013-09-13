53185ee6b1244bc851d7708afc5f692fcfd0788d
mat-lo <matteo.loglio@gmail.com>
2013-09-13 15:18:11 +0200

    first upload

---
+++ TKSensors.cpp
@@ -0,0 +1,435 @@
+/**
+TKSensors.cpp - High level class to read various TKSensors information
+
+@authors
+Arturo Guadalupi <arturoguadalupi@gmail.com>
+Angelo Scialabba <scialabba.angelo@gmail.com>
+
+May/14/2013
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+*/
+
+
+#include <TKSensors.h>
+
+
+extern byte humidityPin = A0;    // sensore di umidità                            
+extern byte analogTempPin = A1;    // sensore di temperatura  
+extern byte brightnessPin = A2;    // sensore di luminosità                        
+extern byte CO2Pin = A3;    // sensore di Co2   
+extern byte digitalTempStatus;
+extern byte analogTempStatus;
+extern byte humidityStatus;
+extern byte altitudeStatus;
+extern byte CO2Status;
+extern byte brightnessStatus;
+extern File outputFile;
+
+
+
+TKSensors::TKSensors()
+{
+_twi = &Wire;
+_sd = &SD;
+
+}
+
+
+void TKSensors::begin(){
+_twi->begin();
+_sd->begin(4);
+sensor_config();
+enableAll();
+}
+
+
+//Functions to set the analog pin to use for each sensor
+//if the user uses single sensor modules
+
+ void TKSensors::setAnalogTempPin(byte pin)
+ {
+ analogTempPin = pin;
+ }
+ 
+void TKSensors::setHumidityPin(byte pin)
+ {
+ humidityPin = pin;
+ }
+ 
+ void TKSensors::setCO2Pin(byte pin)
+ {
+   CO2Pin = pin;
+ }
+ 
+ void TKSensors::setBrightnessPin(byte pin)
+ {
+   brightnessPin = pin;
+ }
+ 
+//Functions for enablig/disabling each sensor
+
+void TKSensors::analogTempOn()
+{
+	analogTempStatus = ON;
+}
+
+void TKSensors::analogTempOff()
+{
+	analogTempStatus = OFF;
+}
+
+void TKSensors::digitalTempOn()
+{
+	digitalTempStatus = ON;
+}
+
+void TKSensors::digitalTempOff()
+{
+	digitalTempStatus = OFF;
+}
+
+void TKSensors::altitudeOn()
+{
+	altitudeStatus = ON;
+}
+
+void TKSensors::altitudeOff()
+{
+	altitudeStatus = OFF;
+}
+
+void TKSensors::humidityOn()
+{
+	humidityStatus = ON;
+}
+
+void TKSensors::humidityOff()
+{
+	humidityStatus = OFF;
+}
+
+void TKSensors::brightnessOn()
+{
+	brightnessStatus = ON;
+}
+
+void TKSensors::brightnessOff()
+{
+	brightnessStatus = OFF;
+}
+
+void TKSensors::CO2On()
+{
+	CO2Status = ON;
+}
+
+void TKSensors::CO2Off()
+{
+	CO2Status = OFF;
+}
+
+void TKSensors::disableAll()
+{
+  CO2Status = OFF;
+  humidityStatus = OFF;
+  brightnessStatus = OFF;
+  altitudeStatus = OFF;
+  digitalTempStatus = OFF;
+  analogTempStatus = OFF;
+ }
+ 
+void TKSensors::enableAll()
+{
+  CO2Status = ON;
+  humidityStatus = ON;
+  brightnessStatus = ON;
+  altitudeStatus = ON;
+  digitalTempStatus = ON;
+  analogTempStatus = ON;
+ }
+
+
+//Functions to read data from sensors
+
+byte TKSensors::analogTemp()
+{
+	if (analogTempStatus) {
+    analogTempValue = analogRead(analogTempPin);
+    return analogTempValue;
+	} else return 0;
+}
+
+byte TKSensors::humidity()
+{
+	if (humidityStatus) {
+    humidityValue = analogRead(humidityPin);
+    return humidityValue;
+	} else return 0;
+}
+
+byte TKSensors::CO2()
+{
+	if (CO2Status) {
+    CO2Value = analogRead(CO2Pin);
+    return CO2Value;
+	} else return 0;
+}
+
+byte TKSensors::brightness()
+{
+	if (brightnessStatus) {
+    brightnessValue = analogRead(brightnessPin);
+    return brightnessValue;
+	} else return 0;
+}
+
+float TKSensors::digitalTemp(byte Tmode)
+{
+    if (digitalTempStatus) {
+    digitalTempValue = MLX(Tmode);
+	return digitalTempValue;
+	} else return 0;
+}
+
+float TKSensors::altitude()	
+{
+
+  int m_altitude, c_altitude;
+  float l_altitude;
+  float f_altitude;
+
+  if (altitudeStatus) {
+  m_altitude = twi_Read(0x01);
+  c_altitude = twi_Read(0x02);
+  l_altitude = (float)(twi_Read(0x03)>>4)/16.0;
+  f_altitude = (float)((m_altitude << 8)|c_altitude) + l_altitude;
+  
+
+  while(check_new() == false);
+  
+  return f_altitude;
+  } else return 0;
+  
+}
+
+float TKSensors::digitalTempAlt()
+{
+  int  m_temp;
+  float  l_temp;
+  float  f_temperature;
+
+  if (altitudeStatus) {
+  m_temp = twi_Read(0x04); //temp, degrees
+  l_temp = (float)(twi_Read(0x05)>>4)/16.0; //temp, fraction of a degree
+  f_temperature = (float)(m_temp + l_temp);
+
+  while(check_new() == false);
+  
+  return f_temperature;
+  } else return 0;
+  
+}
+
+//Function which updates data
+void TKSensors::refreshData()
+{
+  digitalTempValue = digitalTemp(CELSIUS);
+  altitudeValue = altitude();
+  digitalTempAltValue = digitalTempAlt();
+  analogTempValue = analogTemp();
+  humidityValue =	humidity();
+  CO2Value = CO2();
+  brightnessValue = brightness();		
+}
+
+//set the file name of the output logfile
+void TKSensors::setFilename(const char* filename)
+{
+  output_filename = filename;    
+}
+
+byte TKSensors::logAll(){   //log all the active sensors
+  
+  outputFile = _sd->open(output_filename, FILE_WRITE);
+  if (digitalTempStatus){
+    outputFile.print("Temperature (analog) : ");
+    outputFile.println(digitalTempValue);
+	
+  }
+  if (analogTempStatus){
+    outputFile.print("Temperature (digital) : ");
+    outputFile.println(analogTempValue);
+  }
+  if (humidityStatus){
+    outputFile.print("Humidity : ");
+    outputFile.println(humidityValue);
+  }
+  if (altitudeStatus){
+    outputFile.print("Altitude : ");
+    outputFile.println(altitudeValue);
+  }
+  if (CO2Status){
+    outputFile.print("CO2 : ");
+    outputFile.println(CO2Value);
+  }
+  if (brightnessStatus){
+    outputFile.print("Brightness : ");
+    outputFile.println(brightnessValue);
+  }
+  outputFile.println("");
+  outputFile.close();
+  
+}
+
+//*** Function to log each sensor separately
+
+byte TKSensors::logDigitalTemp(){
+  outputFile = _sd->open(output_filename, FILE_WRITE);
+  outputFile.print("Temperature (digital) : ");
+  outputFile.println(digitalTempValue);
+  outputFile.println("");
+  outputFile.close();
+}
+ 
+byte TKSensors::logAnalogTemp(){
+  outputFile = _sd->open(output_filename, FILE_WRITE);
+  outputFile.print("Temperature (analog) : ");
+  outputFile.println(analogTempValue);
+  outputFile.println("");
+  outputFile.close();
+}
+byte TKSensors::logHumidity(){
+  outputFile = _sd->open(output_filename, FILE_WRITE);
+  outputFile.print("Humidity : ");
+  outputFile.println(humidityValue);
+  outputFile.println("");
+  outputFile.close();
+}
+
+byte TKSensors::logAltitude(){
+  outputFile = _sd->open(output_filename, FILE_WRITE);
+  outputFile.print("Altitude : ");
+  outputFile.println(altitudeValue);
+  outputFile.println("");
+  outputFile.close();
+}
+
+byte TKSensors::logCO2(){
+  outputFile = _sd->open(output_filename, FILE_WRITE);
+  outputFile.print("CO2 : ");
+  outputFile.println(CO2Value);
+  outputFile.println("");
+  outputFile.close();
+}
+
+byte TKSensors::logBrightness(){
+  outputFile = _sd->open(output_filename, FILE_WRITE);
+  outputFile.print("Brightness :");
+  outputFile.println(brightnessValue);
+  outputFile.println("");
+  outputFile.close();
+}
+
+byte TKSensors::logDigitalTempAlt(){
+  outputFile = _sd->open(output_filename, FILE_WRITE);
+  outputFile.print("Temperature (Alt.Sensor) : ");
+  outputFile.println(digitalTempValue); 
+  outputFile.println("");
+  outputFile.close();
+}
+
+//** Low level function to handle sensors
+
+byte TKSensors::twi_Read(byte regAddr)
+{
+  _twi->beginTransmission(SENSORADDRESS);
+  _twi->write(regAddr); // Address of CTRL_REG1
+  _twi->endTransmission(false); // Send data to I2C dev with option
+  _twi->requestFrom(SENSORADDRESS, 1); // Request the data...
+  return _twi->read();
+}
+
+void TKSensors::twi_Write(byte regAddr, byte value)
+{
+  _twi->beginTransmission(SENSORADDRESS);
+  _twi->write(regAddr);
+  _twi->write(value);
+  _twi->endTransmission(true);
+}
+
+
+float TKSensors::MLX(byte wantsFahrenheit){  //reads temperature from  the digital sensor
+  
+    int dev = 0x5A<<1;
+    int data_low = 0;
+    int data_high = 0;
+    int pec = 0;
+    
+    _twi->beginTransmission(0x5A);
+    _twi->write(0x07);
+     _twi->endTransmission(false);
+
+    _twi->requestFrom(0x5A, 3);    // request 6 bytes from slave device #2
+
+    int i = 0;
+    char c[] = {0, 0, 0};
+    while(_twi->available())    // slave may send less than requested
+    { 
+    c[i++] = _twi->read();    // receive a byte as character
+    }    
+
+    data_low = c[0];
+    data_high = c[1];
+    pec = c[2];
+    
+    //This converts high and low bytes together and processes temperature, MSB is a error bit and is ignored for temps
+    double tempFactor = 0.02; // 0.02 degrees per LSB (measurement resolution of the MLX90614)
+    double tempData = 0x0000; // zero out the data
+    int frac; // data past the decimal point
+    
+    // This masks off the error bit of the high byte, then moves it left 8 bits and adds the low byte.
+    tempData = (double)(((data_high & 0x007F) << 8) + data_low);
+    tempData = (tempData * tempFactor)-0.01;
+    
+    float celsius = tempData - 273.15;
+    float fahrenheit = (celsius*1.8) + 32;
+	
+	if(wantsFahrenheit) return fahrenheit;
+	else return celsius;
+
+}
+
+boolean TKSensors::check_new()
+{
+
+  if(twi_Read(0x12) == 0x80) // check INT_SOURCE register on
+                             // new data ready (SRC_DRDY)
+  {
+      return true;
+  }
+  else return false;
+}
+
+void TKSensors::sensor_config()
+{
+
+  twi_Write(0x26, 0xB9);
+  twi_Write(0x29, 0x80);
+  twi_Write(0x13, 0x07);
+  twi_Write(0x14, 0xC6);
+  twi_Write(0x15, 0x5B);
+}
---
+++ TKSensors.h
@@ -0,0 +1,141 @@
+/*
+-TKSensors.h -
+High level library to read data from 
+Tinkerkit! Environmental Shield's sensors.
+
+@authors
+Arturo Guadalupi <arturoguadalupi@gmail.com>
+Angelo Scialabba <scialabba.angelo@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+*/
+
+#ifndef TKSensors_h
+#define TKSensors_h
+
+#include <inttypes.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <SD.h>
+
+
+#define SENSORADDRESS  0x60
+#define ON 1
+#define OFF 0
+#define FAHRENHEIT 1
+#define CELSIUS 0
+
+
+
+class TKSensors{
+
+public:
+//**sensor data
+ byte digitalTempStatus;
+ byte analogTempStatus;
+ byte humidityStatus;
+ byte altitudeStatus;
+ byte CO2Status;
+ byte brightnessStatus;
+ 
+ float digitalTempValue;
+ byte analogTempValue;
+ byte humidityValue;
+ float altitudeValue;
+ float digitalTempAltValue;
+ byte CO2Value;
+ byte brightnessValue;
+
+
+ byte analogTempPin;
+ byte humidityPin;  
+ byte CO2Pin;
+ byte brightnessPin;
+ 
+ 
+
+ TKSensors();
+ void begin();
+ 
+
+//** function for reading sensor values
+ byte analogTemp();
+ float digitalTemp(byte Tmode);
+ byte CO2();
+ float altitude();
+ float digitalTempAlt();
+ byte humidity();
+ byte brightness();
+ 
+ 
+ void refreshData();
+
+//**enable/disable sensors
+ void analogTempOn();
+ void analogTempOff();
+
+ void digitalTempOn();
+ void digitalTempOff();
+
+ void altitudeOn();
+ void altitudeOff();
+
+ void humidityOn();
+ void humidityOff();
+
+ void CO2On();
+ void CO2Off();
+
+ void brightnessOn();
+ void brightnessOff();
+ 
+ void disableAll();
+ void enableAll();
+
+//****logging functions
+
+ byte logAll();
+ byte logAnalogTemp();
+ byte logDigitalTemp();
+ byte logHumidity();
+ byte logAltitude();
+ byte logCO2();
+ byte logBrightness();
+ byte logDigitalTempAlt();
+ 
+//set the sensor pin if the user 
+//uses sensor modules
+ void setAnalogTempPin(byte pin);
+ void setHumidityPin(byte pin);
+ void setBrightnessPin(byte pin);
+ void setCO2Pin(byte pin);
+  
+ void setFilename(const char* filename);
+
+private:
+
+const char* output_filename;
+TwoWire * _twi;
+SDClass * _sd;
+File outputFile;
+float MLX(byte wantsFahrenheit);
+byte twi_Read(byte regAddr);
+void twi_Write(byte regAddr, byte value);
+void sensor_config();
+boolean check_new();
+
+};
+#endif 
